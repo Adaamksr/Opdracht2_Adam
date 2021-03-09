f95ad5f662cbb19dc10bd08cfb15d6288c2dcf3f
Adaamksr <adamkasri@gmail.com>
2021-03-09 11:40:18 +0100

    XUnit

---
+++ Services/CalculatorService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Opdracht2_Adam.Services
+{
+    public class CalculatorService : ICalculator
+    {
+        public int Multiply(int g1, int g2)
+        {
+            var result = g1 * g2;
+            return result;
+
+            throw new NotImplementedException();
+        }
+
+        public int Subtract(int g1, int g2)
+        {
+            var result = g1 - g2;
+            return result;
+
+            throw new NotImplementedException();
+        }
+
+        public int Sum(int g1, int g2)
+        {
+            var result = g1 + g2;
+            return result;
+
+            throw new NotImplementedException();
+        }
+
+        public object Multiply(Random random1, Random random2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
